3f762cfd4a591d30efc77b6cefa1e02580b79d7e
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-07-04 23:02:39 +0900

    git init

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import SigninSample from "./pages/samples/SigninSample";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SigninSample from './pages/samples/SigninSample';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/*특정 유알엘에 대해서 특정 페이지 로딩 설정*/}
-        <Route path="/samples/signin" element={<SigninSample />}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                {/*특정 유알엘에 대해서 특정 페이지 로딩 설정*/}
+                <Route
+                    path='/samples/signin'
+                    element={<SigninSample />}
+                ></Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/pages/samples/SigninSample.tsx
+++ src/pages/samples/SigninSample.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import {Avatar, Box, Button, Checkbox, FormControlLabel, Grid, Link, TextField, Typography} from "@mui/material";
+import React from "react";
+import { Avatar, Box, Button, Checkbox, FormControlLabel, Grid, Link, TextField, Typography } from "@mui/material";
 
-import styles from './SigninSample.module.css';
+import styles from "./SigninSample.module.css";
 
 const SigninSample = () => {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
     console.log({
-      email: data.get('email'),
-      password: data.get('password'),
+      email: data.get("email"),
+      password: data.get("password"),
     });
   };
 
@@ -17,14 +17,12 @@ const SigninSample = () => {
     <Box
       sx={{
         marginTop: 8,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
       }}
     >
-      <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-        {/*<LockOutlinedIcon />*/}
-      </Avatar>
+      <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>{/*<LockOutlinedIcon />*/}</Avatar>
       <Typography component="h1" variant="h5" className={styles.wrapper}>
         Sign in
       </Typography>
@@ -49,16 +47,8 @@ const SigninSample = () => {
           id="password"
           autoComplete="current-password"
         />
-        <FormControlLabel
-          control={<Checkbox value="remember" color="primary" />}
-          label="Remember me"
-        />
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          sx={{ mt: 3, mb: 2 }}
-        >
+        <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
+        <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
           Sign In
         </Button>
         <Grid container>

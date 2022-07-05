4c39e7d5ca39d97220601e8a7913256aab912123
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-07-06 08:51:49 +0900

    git commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SigninSample from './pages/samples/SigninSample';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SigninSample from "./pages/samples/SigninSample";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                {/*특정 유알엘에 대해서 특정 페이지 로딩 설정*/}
-                <Route
-                    path='/samples/signin'
-                    element={<SigninSample />}
-                ></Route>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/samples/signin" element={<SigninSample />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

a73208809774b5dae284e5897038b9226f6aadef
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-07-27 23:55:25 +0900

    feat : 라우터 방식 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
+import Routers from "router/Routers";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Routers />;
 }
 
 export default App;

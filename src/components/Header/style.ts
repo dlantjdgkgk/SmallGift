dde517a6bfb5ed5cfad707ce6d80c3d10b5e6dc5
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-07-27 19:16:47 +0900

    fix(Header0 : 헤더 디자인 수정

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,31 +1,27 @@
 import styled from "styled-components";
 
 export const HeaderWrapper = styled.div`
-  min-width: 360px;
-  width: 100vw;
+  width: 100%;
   background-color: #d9d9d9;
   display: flex;
   justify-content: space-between;
   .logo {
-    width: 22vw;
+    width: 80px;
     height: 32px;
     background-color: #a8a8a8;
     margin: 8px 0px 8px 16px;
-    font-size: 2vw;
+    font-size: 8px;
     display: flex;
     justify-content: center;
     align-items: center;
   }
   button {
-    width: 9vw;
+    width: 32px;
     height: 32px;
     border: none;
-    font-size: 2vw;
+    font-size: 8px;
     background-color: #a8a8a8;
-    margin: 8px 12px 8px 0px;
-  }
-  .search {
-    width: 12vw;
+    margin: 8px 16px 8px 0px;
   }
 `;
 

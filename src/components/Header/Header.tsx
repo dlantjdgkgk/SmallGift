85530c031d3d591f1e8e55f2e3e82d262b724301
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-07-27 19:15:24 +0900

    fix(Header) : 헤더 디자인 수정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,10 +7,6 @@ const Header = () => {
       <Styled.HeaderWrapper>
         <div className="logo">Logo</div>
         <div>
-          <button type="button" className="search">
-            Search
-          </button>
-          <button type="button">Cart</button>
           <button type="button">My</button>
         </div>
       </Styled.HeaderWrapper>

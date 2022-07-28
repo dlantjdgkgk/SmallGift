640890f936b311237ffd782fa319b2b4be19d034
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-07-29 00:05:28 +0900

    fix : 라우터 수정

--- src/router/Routers.tsx
+++ src/router/Routers.tsx
@@ -3,15 +3,29 @@ import SignUp from "pages/SignUp";
 import LogIn from "pages/Login";
 import Find from "pages/Find";
 import Main from "pages/Main";
+import Shop from "pages/Shop";
+import Category from "pages/Category";
+import Search from "pages/Search";
+import MyPage from "pages/MyPage";
+import BottomNav from "components/BottomNav/BottomNav";
+import Header from "components/Header/Header";
 
 const Routers = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/find" element={<Find />} />
+        <Route element={<BottomNav />}>
+          <Route element={<Header />}>
+            <Route path="/" element={<Main />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/category" element={<Category />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/mypage" element={<MyPage />} />
+          </Route>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/find" element={<Find />} />
+        </Route>
       </Routes>
     </Router>
   );

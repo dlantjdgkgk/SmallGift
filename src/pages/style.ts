a43b527ecc26a5d7be8e6173198634f2c28b3d34
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-07-29 00:05:21 +0900

    fix : 디자인 수정

--- src/pages/style.ts
+++ src/pages/style.ts
@@ -6,3 +6,8 @@ export const Line = styled.div`
   background-color: #f4f4f4;
   margin-bottom: 30px;
 `;
+export const Navbar = styled.div`
+  width: 100%;
+  height: 212px;
+  background-color: #c1c1c1;
+`;

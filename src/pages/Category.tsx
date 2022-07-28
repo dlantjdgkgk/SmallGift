d07643bf03e16d262688a016108a9ddd3346a462
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-07-29 02:29:46 +0900

    feat : 카테고리 페이지 디자인

--- src/pages/Category.tsx
+++ src/pages/Category.tsx
@@ -1,7 +1,7 @@
-import BottomNav from "components/BottomNav/BottomNav";
+import CategoryPageSection from "components/CategoryPageSection/CategoryPageSection";
 
 const Category = () => {
-  return <div>카테고리 페이지입니다</div>;
+  return <CategoryPageSection />;
 };
 
 export default Category;

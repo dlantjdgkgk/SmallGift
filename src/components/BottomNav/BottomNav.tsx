a4fb6a9b0dc692e52e61e3bdd74e20202d2cd3e6
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-07-29 14:59:17 +0900

    refactor : map 을 이용한 refactoring

--- src/components/BottomNav/BottomNav.tsx
+++ src/components/BottomNav/BottomNav.tsx
@@ -6,39 +6,45 @@ import { Link, Outlet } from "react-router-dom";
 
 const BottomNav = () => {
   const [activeNav, setActiveNav] = useState(1);
-
+  const datas = [
+    {
+      icons: faHome,
+      Links: "/",
+    },
+    {
+      icons: faAlignJustify,
+      Links: "category",
+    },
+    {
+      icons: faSearch,
+      Links: "search",
+    },
+    {
+      icons: faCartShopping,
+      Links: "shop",
+    },
+    {
+      icons: faMedal,
+      Links: "mypage",
+    },
+  ];
   return (
     <>
       <Styled.NavWrapper>
-        <Link to="/" className="nav-link" onClick={() => setActiveNav(1)}>
+        {/* <Link to="/" className="nav-link" onClick={() => setActiveNav(1)}>
           <div>
-            <FontAwesomeIcon icon={faHome} className={activeNav === 1 ? "nav-item active" : "nav-item"} />
+            <FontAwesomeIcon icon={faHome} c  lassName={activeNav === 1 ? "nav-item active" : "nav-item"} />
           </div>
-        </Link>
-
-        <Link to="/category" className="nav-link" onClick={() => setActiveNav(2)}>
-          <div>
-            <FontAwesomeIcon icon={faAlignJustify} className={activeNav === 2 ? "nav-item active" : "nav-item"} />
-          </div>
-        </Link>
-
-        <Link to="/search" className="nav-link" onClick={() => setActiveNav(3)}>
-          <div>
-            <FontAwesomeIcon icon={faSearch} className={activeNav === 3 ? "nav-item active" : "nav-item"} />
-          </div>
-        </Link>
-
-        <Link to="/shop" className="nav-link" onClick={() => setActiveNav(4)}>
-          <div>
-            <FontAwesomeIcon icon={faCartShopping} className={activeNav === 4 ? "nav-item active" : "nav-item"} />
-          </div>
-        </Link>
-
-        <Link to="/mypage" className="nav-link" onClick={() => setActiveNav(5)}>
-          <div>
-            <FontAwesomeIcon icon={faMedal} className={activeNav === 5 ? "nav-item active" : "nav-item"} />
-          </div>
-        </Link>
+        </Link> */}
+        {datas.map(({ icons, Links }, index) => {
+          return (
+            <Link to={Links} className="nav-link" key={index} onClick={() => setActiveNav(index)}>
+              <div>
+                <FontAwesomeIcon icon={icons} className={activeNav === index ? "nav-item active" : "nav-item"} />
+              </div>
+            </Link>
+          );
+        })}
       </Styled.NavWrapper>
       <Outlet />
     </>

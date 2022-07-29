06b839cb58cf5ebe14b8ca6ea0b24664bd253039
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-07-29 14:59:36 +0900

    refactor : 리팩토링

--- src/components/MainPageCategory/MainPageCategory.tsx
+++ src/components/MainPageCategory/MainPageCategory.tsx
@@ -1,31 +1,28 @@
 import * as Styled from "./style";
+import { useNavigate } from "react-router";
 
 const MainPageCategory = () => {
+  const categories = ["전체", "한식", "일식", "중식", "양식", "카페"];
+  const navigate = useNavigate();
   return (
     <>
       <Styled.MainPageCategoryWrapper>
         <p className="categorySearch">카테고리별로 탐색해보세요</p>
         <div className="categories">
-          <div className="FoodCategory">
-            <button type="button" aria-label="Click" />
-            <p>한식</p>
-          </div>
-          <div className="FoodCategory">
-            <button type="button" aria-label="Click" />
-            <p>일식</p>
-          </div>
-          <div className="FoodCategory">
-            <button type="button" aria-label="Click" />
-            <p>중식</p>
-          </div>
-          <div className="FoodCategory">
-            <button type="button" aria-label="Click" />
-            <p>양식</p>
-          </div>
-          <div className="FoodCategory">
-            <button type="button" aria-label="Click" />
-            <p>양식</p>
-          </div>
+          {categories.map((category, index) => {
+            return (
+              <div className="FoodCategory" key={index}>
+                <button
+                  type="button"
+                  aria-label="Click"
+                  onClick={() => {
+                    navigate(`/category?value=${category}`);
+                  }}
+                />
+                <p>{category}</p>
+              </div>
+            );
+          })}
         </div>
       </Styled.MainPageCategoryWrapper>
       <Styled.Line />

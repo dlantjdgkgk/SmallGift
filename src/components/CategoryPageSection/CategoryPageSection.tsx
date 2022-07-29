ede73e3009e1a0c0b54f6126f4abf6969b2f4055
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-07-29 14:59:50 +0900

    feat : 더미데이터 추가

--- src/components/CategoryPageSection/CategoryPageSection.tsx
+++ src/components/CategoryPageSection/CategoryPageSection.tsx
@@ -1,13 +1,42 @@
 import * as Styled from "./style";
-import Select from "react-select";
 import { useState } from "react";
-import { FormatListNumbered } from "@mui/icons-material";
+import queryString from "query-string";
 
 const CategoryPageSection = () => {
   const selectList = ["서울시 강남구 역삼동", "서울시 송파구 가락동"];
-  const selectCategory = ["전체", "한식", "일식", "중식", "양식", "카페"];
-  const [Selected, setSelected] = useState("");
-  const [selectedButton, setSelectedButton] = useState(0);
+  const Categories = ["전체", "한식", "일식", "중식", "양식", "카페"];
+  const [selected, setSelected] = useState("");
+  const queryStringResult = queryString.parse(window.location.search).value;
+  const defaultSelect = queryStringResult;
+  const [selectCategory, setSelectCategory] = useState(defaultSelect);
+
+  const list = [
+    {
+      category: "한식",
+      restaurantName: "쭈꾸미랩소디 강남점",
+      restaurantMenu: "쭈차돌세트,직화쭈꾸미,직화차돌 외",
+    },
+    {
+      category: "일식",
+      restaurantName: "쭈꾸미랩소디 강남점",
+      restaurantMenu: "쭈차돌세트,직화쭈꾸미,직화차돌 외",
+    },
+    {
+      category: "중식",
+      restaurantName: "쭈꾸미랩소디 강남점",
+      restaurantMenu: "쭈차돌세트,직화쭈꾸미,직화차돌 외",
+    },
+    {
+      category: "양식",
+      restaurantName: "쭈꾸미랩소디 강남점",
+      restaurantMenu: "쭈차돌세트,직화쭈꾸미,직화차돌 외",
+    },
+    {
+      category: "카페",
+      restaurantName: "쭈꾸미랩소디 강남점",
+      restaurantMenu: "쭈차돌세트,직화쭈꾸미,직화차돌 외",
+    },
+  ];
 
   return (
     <Styled.CategoryPageSectionWrapper>
@@ -15,7 +44,7 @@ const CategoryPageSection = () => {
         onChange={(e) => {
           setSelected(e.target.value);
         }}
-        value={Selected}
+        value={selected}
       >
         {selectList.map((item, index) => (
           <option value={item} key={index}>
@@ -24,18 +53,18 @@ const CategoryPageSection = () => {
         ))}
       </select>
       <p>근처에 있는 가게를 알려드릴게요</p>
+
       <div className="selectedCategory">
-        {selectCategory.map((category, index) => {
+        {Categories.map((category, index) => {
+          const isSelected = selectCategory === category;
           return (
             <div className="category" key={index}>
               <button
-                style={{
-                  border: selectedButton === index ? "1px solid black" : "none",
-                }}
+                style={isSelected ? { border: "1px solid black" } : undefined}
                 type="button"
                 aria-label="select"
                 onClick={() => {
-                  setSelectedButton(index);
+                  setSelectCategory(category);
                 }}
               >
                 {category}
@@ -44,6 +73,7 @@ const CategoryPageSection = () => {
           );
         })}
       </div>
+
       <div className="restaurants">
         <div className="restaurant">
           <div className="box" />
@@ -55,7 +85,7 @@ const CategoryPageSection = () => {
             <p className="restaurantMenu">쭈차돌세트,직화쭈꾸미,직화차돌 외</p>
           </div>
         </div>
-        <div className="restaurant">
+        {/* <div className="restaurant">
           <div className="box" />
           <div className="restaurantInformation">
             <button className="category" type="button">
@@ -64,8 +94,8 @@ const CategoryPageSection = () => {
             <p className="restaurantName">을지다락 강남</p>
             <p className="restaurantMenu">다락 오므라이스,다락 로-제,가츠산도 외</p>
           </div>
-        </div>
-        <div className="restaurant">
+        </div> */}
+        {/* <div className="restaurant">
           <div className="box" />
           <div className="restaurantInformation">
             <button className="category" type="button">
@@ -74,7 +104,7 @@ const CategoryPageSection = () => {
             <p className="restaurantName">썸띵어바웃커피</p>
             <p className="restaurantMenu">아메리카노,카페라떼,치즈케이크</p>
           </div>
-        </div>
+        </div> */}
       </div>
     </Styled.CategoryPageSectionWrapper>
   );

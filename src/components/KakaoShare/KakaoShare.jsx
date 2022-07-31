8daf699ef9af6fd488996673d90b4a0eb9bb6349
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-07-31 12:19:11 +0900

    feat : kakao share api

---
+++ src/components/KakaoShare/KakaoShare.jsx
@@ -0,0 +1,54 @@
+import { useEffect } from "react";
+
+const KakaoShare = () => {
+  const url = window.location.href;
+  const initKakao = () => {
+    if (window.Kakao) {
+      const kakao = window.Kakao;
+      if (!kakao.isInitialized()) {
+        kakao.init(process.env.REACT_APP_KAKAO_KEY);
+      }
+    }
+  };
+
+  useEffect(() => {
+    initKakao();
+  }, []);
+
+  const shareKakao = () => {
+    window.Kakao.Link.sendDefault({
+      objectType: "feed",
+      content: {
+        title: "딸기 치즈 케익",
+        description: "#케익 #딸기 #삼평동 #카페 #분위기 #소개팅",
+        imageUrl: "http://mud-kage.kakao.co.kr/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png",
+        link: {
+          mobileWebUrl: url,
+          webUrl: url,
+        },
+      },
+      social: {
+        likeCount: 286,
+        commentCount: 45,
+        sharedCount: 845,
+      },
+      buttons: [
+        {
+          title: "웹으로 보기",
+          link: {
+            mobileWebUrl: url,
+            webUrl: url,
+          },
+        },
+      ],
+    });
+  };
+
+  return (
+    <div className="share-node" onClick={shareKakao} aria-hidden="true">
+      <img src="이미지" alt="카카오공유" />
+    </div>
+  );
+};
+
+export default KakaoShare;

28d946513f55ca6fdeb37241c12f7ee30a704202
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-08-01 19:30:42 +0900

    fix : 넷바 디자인 수정

--- src/components/BottomNav/style.ts
+++ src/components/BottomNav/style.ts
@@ -9,18 +9,39 @@ export const NavWrapper = styled.div`
   height: 45px;
   max-width: 768px;
   margin: 0 auto;
-  div {
-    text-align: center;
+  z-index: 1;
+  height: 56px;
+  background-color: white;
+  box-shadow: 0px -2px rgb(0 0 0 / 5%);
+  .iconAndDescription {
     float: left;
     width: 20%;
-    height: 45px;
+    height: 100%;
     line-height: 45px;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    div {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+    }
+    p {
+      font-family: "Noto Sans KR";
+      font-style: normal;
+      font-weight: 400;
+      font-size: 12px;
+      line-height: 17px;
+      letter-spacing: -0.04em;
+      margin-top: 6px;
+    }
   }
   .nav-item {
-    color: grey;
+    color: #bbb;
   }
 
   .active {
-    color: red;
+    color: black;
   }
 `;

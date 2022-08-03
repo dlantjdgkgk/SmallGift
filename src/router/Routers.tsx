f8a353db73acdd984567b9711b72809b45bab935
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-08-03 11:12:32 +0900

    feat : 라우터 수정

--- src/router/Routers.tsx
+++ src/router/Routers.tsx
@@ -10,25 +10,29 @@ import MyPage from "pages/MyPage";
 import CategoryRestaurant from "pages/CategoryRestaurant";
 import BottomNav from "components/BottomNav/BottomNav";
 import Header from "components/Header/Header";
+import ScrollTop from "components/ScrollTop/ScrollTop";
 
 const Routers = () => {
   return (
     <Router>
-      <Routes>
-        <Route element={<BottomNav />}>
+      <ScrollTop>
+        <Routes>
           <Route element={<Header />}>
-            <Route path="/" element={<Main />} />
-            <Route path="/shop" element={<Shop />} />
-            <Route path="/category" element={<Category />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/mypage" element={<MyPage />} />
-            <Route path="/categoryRestaurant" element={<CategoryRestaurant />} />
+            <Route element={<BottomNav />}>
+              <Route path="/" element={<Main />} />
+              <Route path="/shop" element={<Shop />} />
+              <Route path="/category" element={<Category />} />
+              <Route path="/search" element={<Search />} />
+              <Route path="/mypage" element={<MyPage />} />
+              <Route path="/categoryRestaurant" element={<CategoryRestaurant />} />
+            </Route>
           </Route>
-        </Route>
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/find" element={<Find />} />
-      </Routes>
+
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/find" element={<Find />} />
+        </Routes>
+      </ScrollTop>
     </Router>
   );
 };

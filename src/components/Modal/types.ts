1dca7cb287c18736d7b4bab1865b954e51ce8e4c
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-08-03 11:12:00 +0900

    feat : 모달창 타입 지정

---
+++ src/components/Modal/types.ts
@@ -0,0 +1,10 @@
+export interface MovieInfo {
+  setMenuName: string;
+  setMenu: string;
+  price: string;
+}
+export interface Props {
+  menu: MovieInfo;
+  handleModalClose: () => void;
+  modalIsOpen: boolean;
+}

5605e8702919b6540cde01713e88f4b1ed7de053
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-08-06 18:20:10 +0900

    style : 식당디테일 페이지 url 변경

--- src/router/Routers.tsx
+++ src/router/Routers.tsx
@@ -7,10 +7,10 @@ import Shop from "pages/Shop";
 import Category from "pages/Category";
 import Search from "pages/Search";
 import MyPage from "pages/MyPage";
-import CategoryRestaurant from "pages/CategoryRestaurant";
+import RestaurantDetail from "pages/RestaurantDetail";
 import BottomNav from "components/BottomNav/BottomNav";
 import Header from "components/Header/Header";
-import ScrollTop from "components/ScrollTop/ScrollTop";
+import ScrollTop from "components/Header/ScrollTop/ScrollTop";
 
 const Routers = () => {
   return (
@@ -23,7 +23,7 @@ const Routers = () => {
               <Route path="/shop" element={<Shop />} />
               <Route path="/category" element={<Category />} />
               <Route path="/mypage" element={<MyPage />} />
-              <Route path="/categoryRestaurant" element={<CategoryRestaurant />} />
+              <Route path="/restaurantdetail" element={<RestaurantDetail />} />
             </Route>
           </Route>
           <Route element={<BottomNav />}>

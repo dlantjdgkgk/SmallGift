70d7fe994d06d02983b2939aa2283b8bfed88270
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-08-06 18:28:18 +0900

    feat : 식당 디테일 페이지 컴포넌트화( 식당 정보 )

---
+++ src/components/RestaurantInfo/style.ts
@@ -0,0 +1,111 @@
+import styled from "styled-components";
+
+export const RestaurantInfoWrapper = styled.section`
+  .menuAndRestaurantInformation {
+    display: flex;
+    margin-bottom: 32px;
+    button {
+      width: 50%;
+      font-weight: 400;
+      p {
+        font-family: "Noto Sans KR";
+        font-style: normal;
+        font-size: 16px;
+        line-height: 23px;
+        text-align: center;
+        letter-spacing: -0.04em;
+        color: #000000;
+        margin-bottom: 10px;
+      }
+    }
+  }
+  .manyMenu {
+    margin-left: 24px;
+    margin-bottom: 100px;
+  }
+
+  .manyMenuInformation {
+    width: 100%;
+    display: flex;
+    margin-bottom: 24px;
+  }
+  .restaurantInformation {
+    display: flex;
+    flex-direction: column;
+    margin-left: 24px;
+    margin-right: 24px;
+    .addressInformation {
+      display: flex;
+      flex-direction: column;
+      border-bottom: 1px solid #e6e6e6;
+    }
+    .restaurantAddress {
+      display: flex;
+      align-items: center;
+      margin-bottom: 46px;
+    }
+    .address,
+    .hours,
+    .phoneNumberKorean {
+      font-family: "Noto Sans KR";
+      font-style: normal;
+      font-weight: 700;
+      font-size: 16px;
+      line-height: 23px;
+      display: flex;
+      align-items: center;
+      letter-spacing: -0.04em;
+      color: #000000;
+      margin-left: 15.92px;
+    }
+    .map {
+      width: 100%;
+      height: 144px;
+      background-color: black;
+      margin-bottom: 24px;
+    }
+    .restaurantOperatingHours {
+      display: flex;
+      flex-direction: column;
+      border-bottom: 1px solid #e6e6e6;
+      margin-top: 24px;
+      .operatingHours {
+        display: flex;
+        align-items: center;
+        margin-bottom: 6px;
+      }
+      .hourInformation {
+        margin-bottom: 22px;
+        margin-left: 34px;
+        .everyday,
+        .breaktime,
+        .lastOrder,
+        .phoneNumber {
+          font-family: "Noto Sans KR";
+          font-style: normal;
+          font-weight: 400;
+          font-size: 16px;
+          line-height: 23px;
+          display: flex;
+          align-items: center;
+          letter-spacing: -0.04em;
+          color: #000000;
+        }
+      }
+    }
+    .phoneNumberInformation {
+      display: flex;
+      flex-direction: column;
+      margin-top: 24px;
+      margin-bottom: 100px;
+      .phone {
+        display: flex;
+        align-items: center;
+        margin-bottom: 6px;
+      }
+      .phoneNumber {
+        margin-left: 34px;
+      }
+    }
+  }
+`;

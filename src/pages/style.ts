522e083c01e104d2a7906b29b825507f5ad6c8ce
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-08-06 18:29:32 +0900

    style : 컴포넌트 분리로 인한 디자인 변화

--- src/pages/style.ts
+++ src/pages/style.ts
@@ -1,127 +1,26 @@
 import styled from "styled-components";
 
+export const Main = styled.main``;
+
 export const Line = styled.div`
   width: 100%;
   height: 12px;
   background-color: #f4f4f4;
   margin-bottom: 32px;
 `;
-export const Navbar = styled.div`
+export const Navbar = styled.nav`
   width: 100%;
   height: 212px;
   background-color: #c1c1c1;
 `;
 
-export const CategoryRestaurantWrapper = styled.div`
+export const CategoryWrapper = styled.main`
+  margin: 32px 0 0 16px;
+`;
+
+export const CategoryRestaurantWrapper = styled.main`
   position: relative;
   overflow: hidden;
-  .nav {
-    height: 192px;
-    background-color: #c1c1c1;
-    background: linear-gradient(180deg, rgba(0, 0, 0, 0) -55.86%, rgba(0, 0, 0, 0.7) 100%);
-  }
-  .resturantThumbnail {
-    .category {
-      position: absolute;
-      top: 0;
-      width: 32px;
-      height: 20px;
-      background: #5e5e5e;
-      border-radius: 2px;
-      font-family: "Noto Sans KR";
-      font-style: normal;
-      font-weight: 400;
-      font-size: 11px;
-      line-height: 16px;
-      letter-spacing: -0.04em;
-      color: #f4f4f4;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      margin-left: 24px;
-      margin-top: 110px;
-    }
-    .restaurantLike {
-      width: 100%;
-      position: absolute;
-      top: 0;
-      display: flex;
-      justify-content: space-between;
-      margin-top: 4px;
-      padding-right: 16px;
-      padding-left: 24px;
-    }
-    .restaurantName {
-      font-family: "Noto Sans KR";
-      font-style: normal;
-      font-weight: 700;
-      font-size: 24px;
-      line-height: 35px;
-      letter-spacing: -0.04em;
-      color: #ffffff;
-      margin-top: 136px;
-    }
-    .like,
-    .share {
-      width: 32px;
-      height: 32px;
-      background: #737373;
-      font-family: "Noto Sans KR";
-      font-style: normal;
-      font-weight: 400;
-      font-size: 8px;
-      line-height: 12px;
-      text-align: center;
-      letter-spacing: -0.04em;
-      color: #000000;
-      margin-top: 4px;
-    }
-    .like {
-      margin-right: 16px;
-    }
-  }
-
-  .restaurantBestMenu {
-    margin: 24px 0 0 0;
-    .bestMenu {
-      margin-bottom: 16px;
-      font-family: "Noto Sans KR";
-      font-style: normal;
-      font-weight: 700;
-      font-size: 20px;
-      line-height: 29px;
-      letter-spacing: -0.04em;
-      color: #000000;
-      margin-left: 24px;
-    }
-  }
-
-  .bestMenuInformation {
-    width: 100%;
-    display: flex;
-    margin-bottom: 48px;
-  }
-
-  .manyMenu {
-    margin-left: 24px;
-    margin-bottom: 100px;
-  }
-
-  .manyMenuInformation {
-    width: 100%;
-    display: flex;
-    margin-bottom: 24px;
-  }
-
-  .menuInformation {
-    width: 100%;
-    display: flex;
-    margin-left: 24px;
-  }
-  .slick-dots {
-    bottom: 0px;
-    margin-left: 18px;
-  }
 
   .menuImage {
     width: 92px;
@@ -171,166 +70,10 @@ export const CategoryRestaurantWrapper = styled.div`
   }
 
   .line {
-    margin-top: 45px;
+    margin-top: 25px;
     margin-bottom: 16px;
     width: 100%;
     height: 8px;
     background: #f8f8f8;
-  }
-  .menuAndRestaurantInformation {
-    display: flex;
-    margin-bottom: 32px;
-    button {
-      width: 50%;
-      font-weight: 400;
-      p {
-        font-family: "Noto Sans KR";
-        font-style: normal;
-        font-size: 16px;
-        line-height: 23px;
-        text-align: center;
-        letter-spacing: -0.04em;
-        color: #000000;
-        margin-bottom: 10px;
-      }
-    }
-  }
-
-  .restaurantInformation {
-    display: flex;
-    flex-direction: column;
-    margin-left: 24px;
-    margin-right: 24px;
-    .addressInformation {
-      display: flex;
-      flex-direction: column;
-      border-bottom: 1px solid #e6e6e6;
-    }
-    .restaurantAddress {
-      display: flex;
-      align-items: center;
-      margin-bottom: 46px;
-    }
-    .address,
-    .hours,
-    .phoneNumberKorean {
-      font-family: "Noto Sans KR";
-      font-style: normal;
-      font-weight: 700;
-      font-size: 16px;
-      line-height: 23px;
-      display: flex;
-      align-items: center;
-      letter-spacing: -0.04em;
-      color: #000000;
-      margin-left: 15.92px;
-    }
-    .map {
-      width: 100%;
-      height: 144px;
-      background-color: black;
-      margin-bottom: 24px;
-    }
-    .restaurantOperatingHours {
-      display: flex;
-      flex-direction: column;
-      border-bottom: 1px solid #e6e6e6;
-      margin-top: 24px;
-      .operatingHours {
-        display: flex;
-        align-items: center;
-        margin-bottom: 6px;
-      }
-      .hourInformation {
-        margin-bottom: 22px;
-        margin-left: 34px;
-        .everyday,
-        .breaktime,
-        .lastOrder,
-        .phoneNumber {
-          font-family: "Noto Sans KR";
-          font-style: normal;
-          font-weight: 400;
-          font-size: 16px;
-          line-height: 23px;
-          display: flex;
-          align-items: center;
-          letter-spacing: -0.04em;
-          color: #000000;
-        }
-      }
-    }
-    .phoneNumberInformation {
-      display: flex;
-      flex-direction: column;
-      margin-top: 24px;
-      margin-bottom: 100px;
-      .phone {
-        display: flex;
-        align-items: center;
-        margin-bottom: 6px;
-      }
-      .phoneNumber {
-        margin-left: 34px;
-      }
-    }
-  }
-`;
-
-export const CategoryProductWrapper = styled.div`
-  .nav {
-    position: relative;
-    background: #575757;
-    height: 376px;
-    z-index: 1;
-  }
-  .backAndAlertButton {
-    display: flex;
-    justify-content: space-between;
-    margin-left: 16px;
-    margin-right: 16px;
-  }
-  button {
-    width: 32px;
-    height: 32px;
-    background: #a8a8a8;
-    font-family: "Noto Sans KR";
-    font-style: normal;
-    font-weight: 400;
-    font-size: 8px;
-    line-height: 12px;
-    text-align: center;
-    letter-spacing: -0.04em;
-    color: #000000;
-    margin-top: 8px;
-  }
-  .likeAndShareButton {
-    display: flex;
-    margin-top: 8px;
-    button {
-      width: 32px;
-      height: 32px;
-      background: #737373;
-      font-family: "Noto Sans KR";
-      font-style: normal;
-      font-weight: 400;
-      font-size: 8px;
-      line-height: 12px;
-      text-align: center;
-      letter-spacing: -0.04em;
-      color: #000000;
-    }
-    .like {
-      margin-left: 264px;
-      margin-right: 16px;
-    }
-  }
-  .modal {
-    position: absolute;
-    width: 360px;
-    height: 323px;
-    background: #ffffff;
-    border-radius: 16px 16px 0px 0px;
-    z-index: 3;
   }
 `;

7b8169b13b8cbc865b061b4118ab29b1254cb470
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-08-06 18:29:50 +0900

    feat : 서치 페이지 기능 구현

--- src/components/SearchPage/SearchPage.tsx
+++ src/components/SearchPage/SearchPage.tsx
@@ -1,17 +1,16 @@
 import * as Styled from "./style";
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleXmark, faCaretUp } from "@fortawesome/free-solid-svg-icons";
+import { faCaretUp } from "@fortawesome/free-solid-svg-icons";
+import SearchModal from "components/Modal/SearchModal/SerachModal";
+import Portal from "components/Modal/Portal/Portal";
 
 const SearchPage = () => {
   const [text, setText] = useState("");
-  const onChange = (e) => {
-    setText(e.target.value);
-  };
   const [is, setIs] = useState(false);
 
-  const onReset = () => {
-    setText("");
+  const onChange = (e) => {
+    setText(e.target.value);
   };
 
   const datas = [
@@ -29,70 +28,77 @@ const SearchPage = () => {
 
   return (
     <Styled.SearchPageWrapper>
-      <div className="wrapper">
+      {text ? (
         <div className="searchBar">
           <div />
-          <input onChange={onChange} value={text} placeholder="가게명 또는 상품명을 입력하세요" />
+          <input value={text} type="text" placeholder="가게명 또는 상품명을 입력하세요" />
           <button type="button">x</button>
-          {/* <FontAwesomeIcon icon={faCircleXmark} size="1x" color="gray" /> */}
         </div>
-        {is ? (
-          <>
-            <div className="recentSearch">
-              <p className="recent">최근 검색어</p>
-              <p className="available">로그인 후 이용할 수 있어요</p>
+      ) : (
+        <>
+          <div className="wrapper">
+            <div className="searchBar">
+              <div />
+              <input value={text} type="text" placeholder="가게명 또는 상품명을 입력하세요" />
+              <button type="button">x</button>
             </div>
-            <div className="line" />
-          </>
-        ) : (
-          <>
-            <div className="recentSearchLogin">
-              <p className="recent">최근 검색어</p>
-              <button className="totalDelete" type="button" onClick={() => {}}>
-                전체 삭제
-              </button>
-            </div>
-            <div className="records">
-              {datas.map((data, index) => {
-                return (
-                  <div className="record" key={index}>
-                    <p>{data}</p>
-                    <button type="button" onClick={() => {}}>
-                      x
-                    </button>
+
+            {is ? (
+              <>
+                <div className="recentSearch">
+                  <p className="recent">최근 검색어</p>
+                  <p className="available">로그인 후 이용할 수 있어요</p>
+                </div>
+                <div className="line" />
+              </>
+            ) : (
+              <>
+                <div className="recentSearchLogin">
+                  <p className="recent">최근 검색어</p>
+                  <button className="totalDelete" type="button" onClick={() => {}}>
+                    전체 삭제
+                  </button>
+                </div>
+                <div className="records">
+                  {datas.map((data, index) => {
+                    return (
+                      <div className="record" key={index}>
+                        <p>{data}</p>
+                      </div>
+                    );
+                  })}
+                </div>
+                <div className="loginLine" />
+              </>
+            )}
+          </div>
+
+          <div className="popularSearch">
+            <p className="popular">인기 검색어</p>
+            <p className="time">2022년 08월 02일 14:00 기준</p>
+          </div>
+
+          <div className="searchListWrapper">
+            {datas.map((data, index) => {
+              return (
+                <>
+                  <div className="searchList">
+                    <div className="rankAndMenuName">
+                      <div className="rank">{index + 1}</div>
+                      <div className="menuName">{data}</div>
+                    </div>
+                    <div className="iconAndRank">
+                      <FontAwesomeIcon icon={faCaretUp} />
+                      <p className="risingRanking">1</p>
+                    </div>
                   </div>
-                );
-              })}
-            </div>
-            <div className="loginLine" />
-          </>
-        )}
-      </div>
-
-      <div className="popularSearch">
-        <p className="popular">인기 검색어</p>
-        <p className="time">2022년 08월 02일 14:00 기준</p>
-      </div>
-
-      <div className="searchListWrapper">
-        {datas.map((data, index) => {
-          return (
-            <>
-              <div className="searchList">
-                <div className="rankAndMenuName">
-                  <div className="rank">{index + 1}</div>
-                  <div className="menuName">{data}</div>
-                </div>
-                <div className="iconAndRank">
-                  <FontAwesomeIcon icon={faCaretUp} />
-                  <p className="risingRanking">1</p>
-                </div>
-              </div>
-              <div className="boundaryLine" />
-            </>
-          );
-        })}
-      </div>
+                  <div className="boundaryLine" />
+                </>
+              );
+            })}
+          </div>
+        </>
+      )}
     </Styled.SearchPageWrapper>
   );
 };

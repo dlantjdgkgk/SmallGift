0666f2307fff528339b88eb31fb243adb357c1ec
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-08-10 11:37:44 +0900

    feat : 주문 내역 조회 페이지 디자인

---
+++ src/components/OrderList/OrderList.tsx
@@ -0,0 +1,50 @@
+import * as Styled from "./style";
+
+const OrderList = () => {
+  const gifticonList = [
+    {
+      nickName: "기면수",
+      restaurantName: "쭈꾸미랩소디 강남점",
+      setMenu: "쭈차돌세트",
+      price: "15,000",
+    },
+    { nickName: "기면수", restaurantName: "쭈꾸미랩소디 강남점", setMenu: "쭈차돌세트", price: "15,000" },
+    { nickName: "기면수", restaurantName: "쭈꾸미랩소디 강남점", setMenu: "쭈차돌세트", price: "15,000" },
+    { nickName: "기면수", restaurantName: "쭈꾸미랩소디 강남점", setMenu: "쭈차돌세트", price: "15,000" },
+    { nickName: "기면수", restaurantName: "쭈꾸미랩소디 강남점", setMenu: "쭈차돌세트", price: "15,000" },
+    { nickName: "기면수", restaurantName: "쭈꾸미랩소디 강남점", setMenu: "쭈차돌세트", price: "15,000" },
+  ];
+
+  return (
+    <Styled.OrderListWrapper>
+      {gifticonList.map((gifticon, index) => {
+        return (
+          <Styled.OrderSection key={index}>
+            <p className="date">2022년 08월 07일 구매</p>
+            <div className="gifticonInfo">
+              <div className="thumbnail">
+                <img src="/img/foodThumbnail.png" />
+                <div className="customerInfo">🎁{gifticon.nickName}</div>
+              </div>
+              <div className="restaurantInfo">
+                <p className="restaurantName">{gifticon.restaurantName}</p>
+                <p className="setName">{gifticon.setMenu}</p>
+                <p className="price">{gifticon.price}원</p>
+              </div>
+            </div>
+            <div className="optionButton">
+              <button type="button" className="cancel">
+                주문 취소하기
+              </button>
+              <button type="button" className="purchase">
+                재구매하기
+              </button>
+            </div>
+          </Styled.OrderSection>
+        );
+      })}
+    </Styled.OrderListWrapper>
+  );
+};
+
+export default OrderList;

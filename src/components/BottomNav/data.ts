bb6eaf105e899ae6b427cad395cf270554b6cdf8
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-08-30 00:18:50 +0900

    Add : 키 값 추가

--- src/components/BottomNav/data.ts
+++ src/components/BottomNav/data.ts
@@ -5,21 +5,25 @@ export const data = [
     icons: faHome,
     Links: "",
     name: "홈",
+    id: 0,
   },
   {
     icons: faAlignJustify,
     Links: "category",
     name: "카테고리",
+    id: 1,
   },
   {
     icons: faSearch,
     Links: "search",
     name: "검색",
+    id: 2,
   },
 
   {
     icons: faMedal,
     Links: "mypage",
     name: "마이페이지",
+    id: 3,
   },
 ];

34a40df65f30ca4ccafde765798b69ddc2c8e0dd
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-09-06 11:42:12 +0900

    feat : 부드러운 스크롤 기능 추가

--- src/components/Header/ScrollTop/ScrollTop.tsx
+++ src/components/Header/ScrollTop/ScrollTop.tsx
@@ -4,7 +4,10 @@ import { useLocation } from "react-router-dom";
 const ScrollToTop = ({ children }) => {
   const { pathname } = useLocation();
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   }, [pathname]);
 
   return children;

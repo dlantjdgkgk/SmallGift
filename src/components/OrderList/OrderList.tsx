e030ab9aec6dae323f2d10ea9cc2640ca68ff9f8
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-09-08 15:53:42 +0900

    feat : 환불 메세지 경고창 기능 구현

--- src/components/OrderList/OrderList.tsx
+++ src/components/OrderList/OrderList.tsx
@@ -1,3 +1,5 @@
+import { useCallback } from "react";
+import { useNavigate } from "react-router";
 import * as Styled from "./style";
 
 const OrderList = () => {
@@ -14,6 +16,13 @@ const OrderList = () => {
     { nickName: "기면수", restaurantName: "쭈꾸미랩소디 강남점", setMenu: "쭈차돌세트", price: "15,000" },
     { nickName: "기면수", restaurantName: "쭈꾸미랩소디 강남점", setMenu: "쭈차돌세트", price: "15,000" },
   ];
+  const navigate = useNavigate();
+
+  const showConfirm = () => {
+    if (window.confirm("환불 하시겠습니까?")) {
+      navigate("/mypage/refund");
+    }
+  };
 
   return (
     <Styled.OrderListWrapper>
@@ -33,10 +42,16 @@ const OrderList = () => {
               </div>
             </div>
             <div className="optionButton">
-              <button type="button" className="cancel">
+              <button type="button" className="cancel" onClick={showConfirm}>
                 주문 취소하기
               </button>
-              <button type="button" className="purchase">
+              <button
+                type="button"
+                className="purchase"
+                onClick={() => {
+                  navigate("/payment");
+                }}
+              >
                 재구매하기
               </button>
             </div>

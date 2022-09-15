3b54fb8aec24e6f6fa873b4e1959748ffa77b400
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-09-15 15:28:37 +0900

    feat : 환불 디테일 페이지 퍼블리싱

--- src/components/CancellationDetail/CancellationDetail.tsx
+++ src/components/CancellationDetail/CancellationDetail.tsx
@@ -1,5 +1,49 @@
+import * as Styled from "./style";
+
 const CancellationDetail = () => {
-  return <div>취소 환불 상세내역</div>;
+  return (
+    <Styled.CancellationDetailWrapper>
+      <Styled.ProductInfoSection>
+        <h1 className="productInfo">상품 정보</h1>
+        <button type="button" className="orderNumberInfo">
+          <p>
+            <span className="orderNumber">주문번호</span>
+            <span className="number">15930012001</span>
+          </p>
+        </button>
+        <div className="gifticonInfo">
+          <img src="/img/foodThumbnail.png" className="thumbnail" />
+          <div className="setInfo">
+            <p className="restaurantName">쭈꾸미랩소디 강남점</p>
+            <p className="setName">쭈차돌세트</p>
+            <p className="price">15,000원</p>
+          </div>
+        </div>
+      </Styled.ProductInfoSection>
+      <Styled.BoundaryLine />
+      <Styled.RefundInfoSection>
+        <h1>환불 정보</h1>
+        <div className="totalInfo">
+          <label htmlFor="totalPrice" className="totalPrice">
+            총 결제금액
+          </label>
+          <span className="totalMoney">15,000원</span>
+        </div>
+        <div className="refundInfo">
+          <label htmlFor="refundPrice" className="refundPrice">
+            총 환불금액
+          </label>
+          <span className="refundMoney">15,000원</span>
+        </div>
+        <div className="refundMethodInfo">
+          <label htmlFor="refundMethod" className="refundMethod">
+            환불 수단
+          </label>
+          <input type="text" placeholder="신용카드" />
+        </div>
+      </Styled.RefundInfoSection>
+    </Styled.CancellationDetailWrapper>
+  );
 };
 
 export default CancellationDetail;
--- src/components/CancellationDetail/style.ts
+++ src/components/CancellationDetail/style.ts
@@ -1,1 +1,146 @@
 import styled from "styled-components";
+
+export const CancellationDetailWrapper = styled.main``;
+
+export const ProductInfoSection = styled.section`
+  margin-top: 16px;
+  margin-left: 16px;
+  .productInfo {
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 26px;
+    letter-spacing: -0.04em;
+    color: #000000;
+  }
+  .orderNumberInfo {
+    width: 196px;
+    height: 33px;
+    background: #f8f8f8;
+    border-radius: 109px;
+    margin-top: 12px;
+    margin-bottom: 16px;
+  }
+  .orderNumber {
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 20px;
+    letter-spacing: -0.04em;
+    color: #575757;
+  }
+  .number {
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 20px;
+    letter-spacing: -0.04em;
+    color: #949494;
+    margin-left: 8px;
+    margin-right: 21px;
+  }
+  .gifticonInfo {
+    display: flex;
+  }
+  .thumbnail {
+    margin-right: 16px;
+  }
+  .restaurantName {
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 17px;
+    letter-spacing: -0.04em;
+    color: #9f9f9f;
+  }
+  .setName {
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 20px;
+    letter-spacing: -0.04em;
+    color: #000000;
+    margin-bottom: 12px;
+  }
+  .price {
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 20px;
+    letter-spacing: -0.04em;
+    color: #000000;
+  }
+`;
+
+export const BoundaryLine = styled.div`
+  width: 360px;
+  height: 8px;
+  background: #f4f4f4;
+  margin-top: 24px;
+`;
+
+export const RefundInfoSection = styled.section`
+  margin-top: 16px;
+  margin-left: 16px;
+  h1 {
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 26px;
+    letter-spacing: -0.04em;
+    color: #000000;
+    margin-bottom: 16px;
+  }
+  .refundInfo {
+    margin-top: 16px;
+    margin-bottom: 16px;
+  }
+  .totalPrice,
+  .refundPrice,
+  .refundMethod {
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 23px;
+    letter-spacing: -0.04em;
+    color: #000000;
+  }
+  .totalMoney,
+  .refundMoney {
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 23px;
+    letter-spacing: -0.04em;
+    color: #636363;
+    margin-left: 32px;
+  }
+  .refundMoney {
+    font-weight: 700;
+    color: #6600cc;
+  }
+  .refundMethodInfo {
+    display: flex;
+    padding-right: 16px;
+    align-items: center;
+    label {
+      width: 120px;
+    }
+    input {
+      width: 100%;
+      height: 40px;
+      background: #f2f2f2;
+      border-radius: 8px;
+      padding-left: 16px;
+    }
+  }
+`;

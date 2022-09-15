89b27b131e4741805cf4ffcfe7d09161c00a70fa
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-09-15 15:28:23 +0900

    feat : 환불 페이지 퍼블리싱

--- src/components/Cancellation/Cancellation.tsx
+++ src/components/Cancellation/Cancellation.tsx
@@ -1,5 +1,74 @@
+import { useNavigate } from "react-router";
+import * as Styled from "./style";
+
 const Cancellation = () => {
-  return <div>취소</div>;
+  const navigate = useNavigate();
+  return (
+    <Styled.CancellationWrapper>
+      <Styled.CancellationSection>
+        <button
+          type="button"
+          className="orderNumberInfo"
+          onClick={() => {
+            navigate(`/mypage/refund/${15231231231232}`);
+          }}
+        >
+          <p>
+            <span className="orderNumber">주문번호</span>
+            <span className="number">15930012001</span>
+            <img src="/img/Arrow.png" />
+          </p>
+        </button>
+        <div className="gifticonInfo">
+          <img src="/img/foodThumbnail.png" className="thumbnail" />
+          <div className="setInfo">
+            <p className="restaurantName">쭈꾸미랩소디 강남점</p>
+            <p className="setName">쭈차돌세트</p>
+            <p className="price">15,000원</p>
+          </div>
+        </div>
+        <div className="cancelStatus">
+          <img src="/img/Loading.png" />
+          <p className="statusInfo">
+            <span className="status">상태: </span>
+            <span className="loading"> 취소 처리 중</span>
+          </p>
+        </div>
+      </Styled.CancellationSection>
+      <Styled.BoundaryLine />
+      <Styled.CancellationSection>
+        <button
+          type="button"
+          className="orderNumberInfo"
+          onClick={() => {
+            navigate(`/mypage/refund/${123123}`);
+          }}
+        >
+          <p>
+            <span className="orderNumber">주문번호</span>
+            <span className="number">15930012001</span>
+            <img src="/img/Arrow.png" />
+          </p>
+        </button>
+        <div className="gifticonInfo">
+          <img src="/img/foodThumbnail.png" className="thumbnail" />
+          <div className="setInfo">
+            <p className="restaurantName">쭈꾸미랩소디 강남점</p>
+            <p className="setName">쭈차돌세트</p>
+            <p className="price">15,000원</p>
+          </div>
+        </div>
+        <div className="cancelStatus">
+          <img src="/img/Success.png" />
+          <p className="statusInfo">
+            <span className="status">상태: </span>
+            <span className="loading"> 환불완료(신용카드)</span>
+          </p>
+        </div>
+      </Styled.CancellationSection>
+      <Styled.BoundaryLine />
+    </Styled.CancellationWrapper>
+  );
 };
 
 export default Cancellation;
--- src/components/Cancellation/style.ts
+++ src/components/Cancellation/style.ts
@@ -1,1 +1,100 @@
 import styled from "styled-components";
+
+export const CancellationWrapper = styled.main``;
+
+export const CancellationSection = styled.section`
+  margin-left: 16px;
+  .orderNumberInfo {
+    width: 196px;
+    height: 33px;
+    background: #f8f8f8;
+    border-radius: 109px;
+    margin-top: 16px;
+    margin-bottom: 16px;
+  }
+  .orderNumber {
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 20px;
+    letter-spacing: -0.04em;
+    color: #575757;
+  }
+  .number {
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 20px;
+    letter-spacing: -0.04em;
+    color: #949494;
+    margin-left: 8px;
+    margin-right: 21px;
+  }
+  .gifticonInfo {
+    display: flex;
+    margin-bottom: 18px;
+  }
+  .thumbnail {
+    margin-right: 16px;
+  }
+  .restaurantName {
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 17px;
+    letter-spacing: -0.04em;
+    color: #9f9f9f;
+  }
+  .setName {
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 20px;
+    letter-spacing: -0.04em;
+    color: #000000;
+    margin-bottom: 12px;
+  }
+  .price {
+    font-family: "Noto Sans KR";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 20px;
+    letter-spacing: -0.04em;
+    color: #000000;
+  }
+  .cancelStatus {
+    display: flex;
+    align-items: center;
+    img {
+      width: 14px;
+      height: 14px;
+    }
+  }
+  .statusInfo {
+    margin-left: 6px;
+    span {
+      font-family: "Noto Sans KR";
+      font-style: normal;
+      font-weight: 700;
+      font-size: 14px;
+      line-height: 20px;
+      letter-spacing: -0.04em;
+      color: #575757;
+    }
+    .loading {
+      color: gray;
+    }
+  }
+`;
+
+export const BoundaryLine = styled.div`
+  width: 360px;
+  height: 8px;
+  background: #f4f4f4;
+  margin-top: 11px;
+`;

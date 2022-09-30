4580346518c281432643268b4e68c16f3429c90e
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-10-01 03:56:05 +0900

    feat : 카카오 맵 기능 구현

--- src/components/KakaoAPI/KakaoMap/KakaoMap.tsx
+++ src/components/KakaoAPI/KakaoMap/KakaoMap.tsx
@@ -8,26 +8,26 @@ const KakaoMap = () => {
   }, []);
 
   const mapscript = () => {
-    let container = document.getElementById("map");
-    let options = {
+    const container = document.getElementById("map");
+    const options = {
       center: new kakao.maps.LatLng(37.624915253753194, 127.15122688059974),
       level: 5,
     };
 
     const map = new kakao.maps.Map(container, options);
 
-    let markerPosition = new kakao.maps.LatLng(37.62197524055062, 127.16017523675508);
+    const markerPosition = new kakao.maps.LatLng(37.62197524055062, 127.16017523675508);
 
-    let marker = new kakao.maps.Marker({
+    const marker = new kakao.maps.Marker({
       position: markerPosition,
     });
-
     marker.setMap(map);
 
     // 마커에 클릭이벤트를 등록합니다
     kakao.maps.event.addListener(marker, "click", function () {
       // 마커 위에 인포윈도우를 표시합니다
-      let link = "http://www.naver.com/";
+      const url = "https://map.kakao.com/link/search/";
+      const link = `${url}서울 강남구 강남대로98길 12-3 1층`;
       window.open(link);
     });
   };

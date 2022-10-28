b4341cc66f2da9da77ccd601ab300349214e83e0
moon115599 <76196460+moon115599@users.noreply.github.com>
2022-10-29 03:39:00 +0900

    feat(아이디 찾기): page 생성

--- src/pages/FindID.tsx
+++ src/pages/FindID.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Avatar, Box, Container, CssBaseline, Grid, Link, Typography } from "@mui/material";
+import FindIDForm from "../../../components/find/findID/FindIDForm";
 
 const FindID = () => {
-  return <div />;
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }} />
+        <Typography component="h1" variant="h5">
+          아이디 찾기
+        </Typography>
+        <FindIDForm />
+      </Box>
+    </Container>
+  );
 };
 
 export default FindID;

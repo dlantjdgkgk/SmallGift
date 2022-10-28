64f4392387ade9024a7d61db1eb380c8b3e7cbc4
moon115599 <76196460+moon115599@users.noreply.github.com>
2022-10-29 01:33:01 +0900

    feat(회원api): 관리자에서 만든 회원관리 관련 api 옮기기

---
+++ src/api/user/signUp.ts
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { api } from "../server/Api";
+
+export const axiosEmailCheck = async (email) => {
+  try {
+    const response = await api.post(`../api/user/${email}/exists`, {});
+    if (!response.success) {
+      alert(response.message);
+      return false;
+    }
+    console.log(response);
+    return true;
+  } catch (error) {
+    console.log("중복 확인에 실패하였습니다.");
+    console.log(error);
+    return false;
+  }
+};
+export const axiosUsernameCheck = async (username) => {
+  try {
+    const response = await api.post(`../api/user/${username}/exists`, {});
+    if (!response.success) {
+      alert(response.message);
+      return false;
+    }
+    return true;
+  } catch (error) {
+    console.log("중복 확인에 실패하였습니다.");
+    console.log(error);
+    return false;
+  }
+};
+
+export const axiosSignUpUser = async (dataTosubmit) => {
+  try {
+    const response = await api.post("../api/user/signup", dataTosubmit);
+    if (!response.success) {
+      alert(response.message);
+      return false;
+    }
+    return true;
+  } catch (error) {
+    alert("회원가입에 실패하였습니다.");
+    return false;
+  }
+};

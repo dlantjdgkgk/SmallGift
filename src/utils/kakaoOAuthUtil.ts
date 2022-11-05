7a14e3f0533772bfc5454fdb17002120c29ae6c0
moon115599 <76196460+moon115599@users.noreply.github.com>
2022-11-05 16:21:09 +0900

    fix(카카오 로그인): redirect URL 프런트 url로 변경

--- src/utils/kakaoOAuthUtil.ts
+++ src/utils/kakaoOAuthUtil.ts
@@ -1,4 +1,4 @@
 const REST_API_KEY = "a865c4442f6a8a5ad97d0b11c0d1e379";
-const REDIRECT_KEY = `${process.env.REACT_APP_BASE_URL}/auth/kakao/callback`;
+const REDIRECT_KEY = `${process.env.REACT_APP_HOME_URL}/auth/kakao/callback`;
 
 export const kakaOauthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_KEY}&response_type=code`;

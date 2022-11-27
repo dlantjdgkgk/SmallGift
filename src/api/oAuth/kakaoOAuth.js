8dd6f24140cbfb769bb23e33dd5535217922cfd0
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-11-28 00:07:51 +0900

    refactor: 코드 수정

--- src/api/oAuth/kakaoOAuth.js
+++ src/api/oAuth/kakaoOAuth.js
@@ -4,14 +4,15 @@ import Swal from "sweetalert2";
 export const axiosKakaoLogin = async (code) => {
   try {
     const response = await accessApi.get(`/api/user/oauth/kakao/token?code=${code}`);
+    console.log(response);
     if (response.status === 200) {
       const today = new Date();
       const TOKEN_TIME_OUT = 600 * 1000;
       const expireAccessToken = today.getTime() + TOKEN_TIME_OUT;
 
-      window.localStorage.setItem("acessToken", response.data.jwtAccessToken);
+      window.localStorage.setItem("acessToken", response.data.data.jwtAccessToken);
       window.localStorage.setItem("expireAccessToken", expireAccessToken);
-      window.localStorage.setItem("memberId", response.data.memberId);
+      window.localStorage.setItem("memberId", response.data.data.memberId);
 
       Swal.fire({
         text: "로그인에 성공하였습니다..",

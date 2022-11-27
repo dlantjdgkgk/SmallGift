2a06b2b355f3c91d7ef8f63b2cc041e161044443
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-11-28 02:13:42 +0900

    test

--- src/utils/kakaoOAuthUtil.ts
+++ src/utils/kakaoOAuthUtil.ts
@@ -1,4 +1,4 @@
 const REST_API_KEY = process.env.REACT_APP_REST_API_KEY;
-const REDIRECT_KEY = "http://localhost:3000/auth/kakao/callback";
+const REDIRECT_KEY = process.env.REACT_APP_REDIRECT_KEY;
 
 export const kakaOauthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_KEY}&response_type=code`;

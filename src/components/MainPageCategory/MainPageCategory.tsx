1fd337d1d357e9ed4fc1962054e7f7f0436551c8
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-11-30 00:49:00 +0900

    style: 코드 정리

--- src/components/MainPageCategory/MainPageCategory.tsx
+++ src/components/MainPageCategory/MainPageCategory.tsx
@@ -1,26 +1,8 @@
 import * as Styled from "./style";
 import { Link } from "react-router-dom";
 import { categories, images } from "./datas";
-import { apiInstance } from "../../api/setting";
-import { useEffect, useState } from "react";
 
 const MainPageCategory = (): JSX.Element => {
-  const memberId = 16;
-  const [locate, setLocate] = useState(null);
-
-  const userLocateAPI = async (): Promise<void> => {
-    try {
-      const res = await apiInstance.get(`/api/user/locate?memberId=${memberId}`);
-      setLocate(res.data);
-    } catch (error) {
-      throw new Error("check the network response");
-    }
-  };
-
-  useEffect(() => {
-    userLocateAPI();
-  }, []);
-
   return (
     <>
       <Styled.MainPageCategoryWrapper>
@@ -28,12 +10,7 @@ const MainPageCategory = (): JSX.Element => {
         <section className="categories">
           {categories.map((category) => {
             return (
-              <Link
-                to={`/category?value=${category.category}`}
-                key={category.id}
-                style={{ color: "black" }}
-                state={locate}
-              >
+              <Link to={`/category?value=${category.category}`} key={category.id} style={{ color: "black" }}>
                 <article className="foodCategory">
                   <img src={images[category.id]} alt="" />
                   <p>{category.category}</p>

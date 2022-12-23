8e0ef3c440e5189208cb1ee457dbeaeaad3c1e4c
dlantjdgkgk <dlantjdgkgk@naver.com>
2022-12-23 18:23:08 +0900

    feat : 글로벌 스타일 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeProvider as StyledComponentsThemeProvider } from "styled-components";
+import Theme from "styles/theme";
+import GlobalStyle from "styles/GlobalStyle";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "./theme";
 import "./index.css";
@@ -7,8 +10,11 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <App />
-  </ThemeProvider>,
+  <StyledComponentsThemeProvider theme={Theme}>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </StyledComponentsThemeProvider>,
 );
